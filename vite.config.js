d11539bf24301ebe2885f1266801e95b5455f48e
CrazySpottedDove <3230104178@zju.edu.cn>
2025-02-17 08:03:08 +0800

    fix vite.config.js : import tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
