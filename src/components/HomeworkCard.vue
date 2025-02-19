91aded9ec3e1ab927a2a02a67342f35097d9088f
CrazySpottedDove <3230104178@zju.edu.cn>
2025-02-19 15:08:12 +0800

    finish submit homework function

--- src/components/HomeworkCard.vue
+++ src/components/HomeworkCard.vue
@@ -17,6 +17,11 @@
         <br>
         <UploadsCard :uploads="homework.uploads" :title="homework.course" />
     </li>
+    <div v-if="submitting" class="fixed inset-0 z-50 dark:bg-zinc-900 bg-opacity-80 flex flex-col">
+        <div class="flex-1 flex justify-center items-center">
+            <p class="text-white font-bold text-xl">上传中...</p>
+        </div>
+    </div>
     <br>
 </template>
 
@@ -35,7 +40,7 @@ function submit(id) {
         if (file_path === null) {
             return;
         }
-        const submitFilePromise = invoke('submit_file',{file_path: file_path});
+        const submitFilePromise = invoke('submit_file', { file_path: file_path });
         const comment = window.prompt("请输入备注(可不输入)") || "";
         submitFilePromise.then((file_id) => {
             submitting.value = true;
@@ -47,6 +52,7 @@ function submit(id) {
                     title: '提交成功',
                     timer: 1500,
                 })
+                props.homework.submitted = true;
             }).catch((e) => {
                 submitting.value = false;
                 Swal.fire({
