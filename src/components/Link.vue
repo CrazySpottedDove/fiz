61458ce8fd1908de43d567c5a2019b46ac4c63d0
CrazySpottedDove <3230104178@zju.edu.cn>
2025-02-20 12:03:33 +0800

    添加登出确认

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -5,7 +5,8 @@
             ? 'dark:text-blue-400 text-blue-600 border-b-2 border-blue-400 dark:border-blue-600'
             : 'dark:text-zinc-300 text-zinc-600',
         addClass
-    ]" :aria-current="isActive ? 'page' : undefined" v-bind="props" :aria-label="`Go to ${text} page`" @click="() => $router.push(href)">
+    ]" :aria-current="isActive ? 'page' : undefined" v-bind="props" :aria-label="`Go to ${text} page`"
+        @click="handleChangeRoute">
         {{ text }}
     </span>
 </template>
@@ -14,7 +15,8 @@
 import { computed } from 'vue';
 import { useRoute } from 'vue-router';
 import { toRefs } from 'vue';
-
+import router from '../router';
+import Swal from 'sweetalert2';
 const props = defineProps({
     text: String,
     addClass: String,
@@ -33,5 +35,22 @@ const isActive = computed(() => {
         return route.path.includes(href.value);
     }
 });
-
+const handleChangeRoute = () => {
+    if (text.value === "登出") {
+        Swal.fire({
+            title: '确认登出?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: '确认',
+            cancelButtonText: '取消'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // 执行登出操作
+                router.push(href.value);
+            }
+        });
+    } else {
+        router.push(href.value);
+    }
+}
 </script>
