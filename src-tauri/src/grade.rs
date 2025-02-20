6946fdf39b33076161ecdece63dd3c883bee873d
CrazySpottedDove <3230104178@zju.edu.cn>
2025-02-20 18:29:20 +0800

    fix grade fault caused by no CJ

--- src-tauri/src/grade.rs
+++ src-tauri/src/grade.rs
@@ -137,12 +137,17 @@ impl Session {
             match json["data"]["items"].as_array() {
                 Some(grades_json) => {
                     for grade_json in grades_json {
+
                         if !grade_json["BZ"].is_null()
                             && grade_json["BZ"].as_str().unwrap() == "弃修"
                         {
                             continue;
                         }
-                        let grade = grade_json["CJ"].as_u64().unwrap().to_string();
+                        let grade = if grade_json["CJ"].is_null(){
+                            continue;
+                        }else{
+                            grade_json["CJ"].as_u64().unwrap().to_string()
+                        };
                         let name = grade_json["KCMC"].as_str().unwrap().to_string();
                         let credit = grade_json["XF"].as_str().unwrap().to_string();
                         let gpa = grade_json["JD"].as_str().unwrap().parse::<f64>().unwrap();
